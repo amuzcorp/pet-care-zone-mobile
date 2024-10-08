aed5377f5586e2a449ae4c649c1386cfa29350f2
zaewankim <complete@amuz.co.kr>
2024-10-08 19:29:18 +0900

    [Modify] - image 경로 수정

--- lib/constants/image_constants.dart
+++ lib/constants/image_constants.dart
@@ -4,6 +4,7 @@ class ImageConstants {
   static const String productConnectionGuide2 = 'assets/images/product_connection/2_check_pincode.png';
   static const String productConnectionGuide3 = 'assets/images/product_connection/3_input_pincode.png';
   static const String productConnectionGuide4 = 'assets/images/product_connection/4_check_pincode.png';
-  static const String productConnectionGuide5 = 'assets/images/product_connection/5_provision_product.png';
-  static const String productConnectionGuide6 = 'assets/images/product_connection/6_device_name_edit_product.png';
+  static const String productConnectionGuide5 = 'assets/images/product_connection/5_connection_pincode.png';
+  static const String productConnectionGuide6 = 'assets/images/product_connection/6_provision_product.png';
+  static const String productConnectionGuide7 = 'assets/images/product_connection/7_device_name_edit_product.png';
 }
