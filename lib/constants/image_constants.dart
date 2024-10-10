c3617fe45b0ab4946fe44b2444224452c804225b
zaewankim <complete@amuz.co.kr>
2024-10-10 15:19:39 +0900

    [Modify] - image 경로 수정

--- lib/constants/image_constants.dart
+++ lib/constants/image_constants.dart
@@ -1,10 +1,10 @@
 class ImageConstants {
-  static const String productConnectionGuide0 = 'assets/images/product_connection/0_check_power.png';
-  static const String productConnectionGuide1 = 'assets/images/product_connection/1_on_device_list.png';
-  static const String productConnectionGuide2 = 'assets/images/product_connection/2_check_pincode.png';
-  static const String productConnectionGuide3 = 'assets/images/product_connection/3_input_pincode.png';
+  static const String productConnectionGuide1 = 'assets/images/product_connection/1_initial_device_bg.png';
+  static const String productConnectionGuide2 = 'assets/images/product_connection/2_check_power.png';
+  static const String productConnectionGuide3 = 'assets/images/product_connection/3_on_device_list.png';
   static const String productConnectionGuide4 = 'assets/images/product_connection/4_check_pincode.png';
-  static const String productConnectionGuide5 = 'assets/images/product_connection/5_connection_pincode.png';
-  static const String productConnectionGuide6 = 'assets/images/product_connection/6_provision_product.png';
-  static const String productConnectionGuide7 = 'assets/images/product_connection/7_device_name_edit_product.png';
+  static const String productConnectionGuide5 = 'assets/images/product_connection/5_input_pincode.png';
+  static const String productConnectionGuide6 = 'assets/images/product_connection/6_add_product.png';
+  static const String productConnectionGuide7 = 'assets/images/product_connection/7_provision_product.png';
+  static const String productConnectionGuide8 = 'assets/images/product_connection/8_device_name_edit_product.png';
 }
